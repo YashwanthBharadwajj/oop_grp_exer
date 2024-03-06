245de30642703e4fe6d57787dbf1ba1fc50f8f0f
Leelamahesh Kodi <kodi1@usf.edu>
2024-03-06 12:07:51 -0500

    class added

---
+++ Brand.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace oop_grp_exer
+{
+ public class Vehicle
+        {
+            // Variables
+            private string brand;
+            private string model;
+            private int year;
+            private bool isRunning;
+
+            // Constructor
+            public Vehicle(string brand, string model, int year)
+            {
+                this.brand = brand;
+                this.model = model;
+                this.year = year;
+                this.isRunning = false; // Vehicle starts not running
+            }
+        }
+}
